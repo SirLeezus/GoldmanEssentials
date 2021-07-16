a7fda18a81fb6efa05463c5ce9e8423e761b9777
Lee <leecook808@gmail.com>
2021-07-16 14:37:28 -0700

    Spawner listener update

--- src/main/java/lee/code/essentials/listeners/SpawnerListener.java
+++ src/main/java/lee/code/essentials/listeners/SpawnerListener.java
@@ -3,6 +3,7 @@ package lee.code.essentials.listeners;
 import lee.code.essentials.GoldmanEssentials;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.block.CreatureSpawner;
@@ -12,7 +13,12 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryType;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
 
 public class SpawnerListener implements Listener {
 
@@ -30,6 +36,48 @@ public class SpawnerListener implements Listener {
                 EntityType mob = cs.getSpawnedType();
                 ItemStack spawner = plugin.getPU().createSpawner(mob);
                 world.dropItemNaturally(location, spawner);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onSpawnerPlace(BlockPlaceEvent e) {
+        Player player = e.getPlayer();
+        Block block = e.getBlock();
+
+        if (block.getState() instanceof CreatureSpawner cs) {
+            ItemStack spawner = player.getInventory().getItemInMainHand();
+            BlockStateMeta spawnerMeta = (BlockStateMeta) spawner.getItemMeta();
+            if (spawnerMeta != null) {
+                CreatureSpawner spawnerCS = (CreatureSpawner) spawnerMeta.getBlockState();
+                cs.setSpawnedType(spawnerCS.getSpawnedType());
+                cs.update();
+            }
+        }
+    }
+
+    @EventHandler
+    public void onSpawnerExplode(EntityExplodeEvent e) {
+        GoldmanEssentials plugin = GoldmanEssentials.getPlugin();
+        World world = e.getLocation().getWorld();
+
+        for (Block block : e.blockList()) {
+            if (block.getState() instanceof CreatureSpawner cs) {
+                EntityType mob = cs.getSpawnedType();
+                ItemStack spawner = plugin.getPU().createSpawner(mob);
+                world.dropItemNaturally(block.getLocation(), spawner);
+            }
+        }
+    }
+
+    @EventHandler
+    public void onSpawnerAnvilRename(InventoryClickEvent e) {
+        if (e.getCurrentItem() != null && e.getCurrentItem().getType() != Material.AIR) {
+            if (e.getInventory().getType() != InventoryType.ANVIL) return;
+            if (e.getSlotType() != InventoryType.SlotType.RESULT) return;
+            ItemStack clicked = e.getInventory().getItem(0);
+            if (clicked != null) {
+                if (clicked.getType().equals(Material.SPAWNER)) e.setCancelled(true);
             }
         }
     }
