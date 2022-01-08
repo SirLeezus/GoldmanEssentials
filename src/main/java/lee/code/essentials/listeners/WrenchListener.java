c1ec5dfa154fd53730edb9f1087f14b7f6b9da1a
Lee <leecook808@gmail.com>
2022-01-08 01:32:28 -0800

    Wrench fixes

--- src/main/java/lee/code/essentials/listeners/WrenchListener.java
+++ src/main/java/lee/code/essentials/listeners/WrenchListener.java
@@ -11,7 +11,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.inventory.DoubleChestInventory;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
